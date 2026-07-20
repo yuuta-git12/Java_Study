9568bf7f329e056fc566c41905d3aa9ea8a9d187
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2026-07-21 08:13:48 +0900

    [main]-[add]：クラス定義用のサンプルコード追加

--- projava/src/main/java/org/example/Main.java
+++ projava/src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import org.example.code.*;
 
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
@@ -6,7 +7,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
         // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
+        Item item = new Item();
+        // itemのメンバ変数nameに値を設定
+        item.setName("Laptop");
+        System.out.printf("Hello and welcome! Item: %s", item.getName());
+
+        // エラーになる書き方
+        // item.name = "Desktop";
 
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
---
+++ projava/src/main/java/org/example/code/Item.java
@@ -0,0 +1,11 @@
+package org.example.code;
+
+public class Item {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
